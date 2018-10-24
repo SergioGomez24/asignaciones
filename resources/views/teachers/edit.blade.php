e13d249f82d0e3450918957bb3a361e868f402eb
SergioGomez24 <sergio_xiclanero_92@hotmail.es>
2018-10-24 13:55:21 +0200

    form

--- resources/views/teachers/edit.blade.php
+++ resources/views/teachers/edit.blade.php
@@ -13,37 +13,37 @@
 
             <div class="form-group">
                <label for="name">Nombre</label>
-               <input type="text" name="name" id="name" value={{$profesor->name}} class="form-control">
+               <input type="text" name="name" id="name" value="{{$profesor->name}}" class="form-control">
             </div>
 
             <div class="form-group">
                <label for="dni">DNI</label>
-               <input type="text" name="dni" id="dni" value = {{$profesor->dni}} class="form-control">
+               <input type="text" name="dni" id="dni" value="{{$profesor->dni}}" class="form-control">
             </div>
 
             <div class="form-group">
                <label for="category">Categoría</label>
-               <input type="text" name="category" id="category" value={{$profesor->category}} class="form-control">
+               <input type="text" name="category" id="category" value="{{$profesor->category}}" class="form-control">
             </div>
 
             <div class="form-group">
                <label for="area">Área</label>
-               <input type="text" name="area" id="area" value={{$profesor->area}} class="form-control">
+               <input type="text" name="area" id="area" value="{{$profesor->area}}" class="form-control">
             </div>
 
             <div class="form-group">
                <label for="cInitial">Créditos iniciales</label>
-               <input type="number" name="cInitial" id="cInitial" value={{$profesor->cInitial}} class="form-control">
+               <input type="number" name="cInitial" id="cInitial" value="{{$profesor->cInitial}}" class="form-control">
             </div>
 
             <div class="form-group">
                <label for="dateCategory">Fecha inicio categoria</label>
-               <input type="date" name="dateCategory" id="dateCategory" value={{$profesor->dateCategory}} class="form-control">
+               <input type="date" name="dateCategory" id="dateCategory" value="{{$profesor->dateCategory}}" class="form-control">
             </div>
 
             <div class="form-group">
                <label for="dateUCA">Fecha inicio UCA</label>
-               <input type="date" name="dateUCA" id="dateUCA" value={{$profesor->dateUCA}} class="form-control">
+               <input type="date" name="dateUCA" id="dateUCA" value="{{$profesor->dateUCA}}" class="form-control">
             </div>
 
             <div class="form-group text-center">
