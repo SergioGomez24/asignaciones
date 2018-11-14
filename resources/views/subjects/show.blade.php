1dff5dd179319c59c635bf850f2bb1bf6865e346
SergioGomez24 <sergio_xiclanero_92@hotmail.es>
2018-11-14 13:41:39 +0100

    show

--- resources/views/subjects/show.blade.php
+++ resources/views/subjects/show.blade.php
@@ -7,21 +7,22 @@
             <h2> {{$asignatura->name}} </h2>
           </div>
           <div class="card-body">
-      		  <h5> Código: {{$asignatura->code}} </h5>
-      		  <h5> Titulación: {{$asignatura->certification}} </h5>
-      		  <h5> Area: {{$asignatura->area}} </h5>
-      		  <h5> Campus: {{$asignatura->campus}} </h5>
-      		  <h5> Centro: {{$asignatura->center}} </h5>
-      		  <h5> Créditos teoria: {{$asignatura->cTheory}} </h5>
-      		  <h5> Créditos seminario: {{$asignatura->cSeminar}} </h5>
-      		  <h5> Créditos prácticas: {{$asignatura->cPractice}} </h5>
-      		  <h5> Duración: {{$asignatura->duration}} </h5>
-      		  <h5> Curso en la que se imparte: {{$asignatura->imparted}} </h5>
-      		  <h5> Tipo de asignatura: {{$asignatura->typeSubject}} </h5>
-            <h5> Coordinador: {{$asignatura->coordinator}} </h5>
-
-      		  <a class="btn btn-default" href="{{ url('/subjects') }}" role="button" style="margin-right: 15px">Volver al listado
-      		  </a>
+            <div class="col-md-5" style="float: left;">
+              <h5> Código: {{$asignatura->code}}</h5>
+      		    <h5> Titulación: {{$asignatura->certification}} </h5>
+      		    <h5> Area: {{$asignatura->area}} </h5>
+      		    <h5> Campus: {{$asignatura->campus}} </h5>
+      		    <h5> Centro: {{$asignatura->center}} </h5>
+              <h5> Duración: {{$asignatura->duration}} </h5>
+              <h5> Curso en la que se imparte: {{$asignatura->imparted}} </h5>
+              <h5> Tipo de asignatura: {{$asignatura->typeSubject}} </h5>
+              <h5> Coordinador: {{$asignatura->coordinator}} </h5>
+            </div>
+            <div class="col-md-5" style="float: right;">
+              <h5> Créditos teoria: {{$asignatura->cTheory}} </h5>
+              <p><h5> Créditos seminario: {{$asignatura->cSeminar}} </h5></p>
+              <p><h5> Créditos prácticas: {{$asignatura->cPractice}} </h5></p>
+            </div>
           </div>
         </div>
 		</div>
--- resources/views/teachers/show.blade.php
+++ resources/views/teachers/show.blade.php
@@ -13,22 +13,6 @@
       		  <h5> Créditos iniciales: {{$profesor->cInitial}} </h5>
       		  <h5> Fecha inicio categoría: {{$profesor->dateCategory}} </h5>
       		  <h5> Fecha inicio UCA: {{$profesor->dateUCA}} </h5>
-
-      		  <a class="btn btn-warning" href="{{ url('/teachers/edit/'.$profesor->id) }}" role="button" style="margin-right: 15px">
-        	   Editar profesor
-      		  </a>
-      		
-      		  <a class="btn btn-default" href="{{ url('/teachers') }}" role="button" style="margin-right: 15px">
-        	   Volver al listado
-      		  </a>
-
-      		  <form action="{{action('TeachersController@deleteTeacher', $profesor->id)}}" method="POST" style="display:inline">
-        		  {{ method_field('DELETE') }}
-        		  {{ csrf_field() }}
-        		  <button type="submit" class="btn btn-default" style="display:inline">
-          			 Eliminar profesor
-        		  </button>
-      		  </form>
           </div>
         </div>
 		</div>
