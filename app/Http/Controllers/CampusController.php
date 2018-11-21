fcbd575e90a45d8c43d6a1b9564c1a6814470882
SergioGomez24 <sergio_xiclanero_92@hotmail.es>
2018-11-22 00:53:42 +0100

    create campus

---
+++ app/Http/Controllers/CampusController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Campus;
+use Notification;
+
+class CampusController extends Controller
+{
+	public function getIndex() 
+    {
+    	$arrayCampus = Campus::all();
+
+    	return view('settings.campus.index', ['arrayCampus' => $arrayCampus]);
+    }
+
+    public function getShow($id) 
+    {
+    	$campus = Campus::findOrFail($id);
+
+    	return view('settings.campus.show', ['campus' => $campus]);
+    }
+
+	public function getCreate() 
+    {
+    	return view('settings.campus.create');
+    }
+
+    public function postCreate(Request $request) 
+     {
+        $c = new Campus;
+        $c->name = $request->input('name');
+        $c->save();
+        Notification::success('El campus se ha guardado exitosamente!');
+        return redirect('/settings/campus');
+    }
+
+    public function getEdit($id) 
+    {
+		$campus = Campus::findOrFail($id); 
+		    	
+     	return view('settings.campus.edit', ['campus' => $campus]);
+    }
+
+    public function putEdit(Request $request, $id)
+    {
+        $c = Campus::findOrFail($id);
+        $c->name = $request->input('name');
+        $c->save();
+        Notification::success('El campus ha sido modificada exitosamente!');
+        return redirect('/settings/campus/show/'.$id);
+    }
+
+    public function deleteCategory(Request $request, $id)
+    {
+        $c = Campus::findOrFail($id);
+        $c->delete();
+        Notification::success('El campus fue eliminada exitosamente!');
+        return redirect('/settings/campus');
+    }
+
+}
